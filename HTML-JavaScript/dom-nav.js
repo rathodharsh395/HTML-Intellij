c80b57e8a493ee977a58200e12ccaee73bd9b04f
ℍℝ <rathodharsh395@gmail.com>
2023-10-18 12:38:53 +0530

    Up to !important Hack

---
+++ HTML-JavaScript/dom-nav.js
@@ -0,0 +1,34 @@
+
+//  document.getElementById("child-C").parentNode.style.background = "red"
+//  var a = document.getElementById("main").parentElement;
+//  var a = document.getElementById("main").parentNode;
+
+//  document.getElementById("inner").childNodes[3].style.background = "green";
+//  var a = document.getElementById("inner").childNodes[3];
+
+//   document.getElementById("outer").firstElementChild.style.background = "pink";
+//   var a = document.getElementById("outer").firstElementChild;
+
+//   document.getElementById("inner").lastElementChild.style.background = "yellow";
+//   var a = document.getElementById("inner").lastElementChild;
+
+//   document.getElementById("inner").lastElementChild.style.background = "yellow";
+//   document.getElementById("inner").lastChild.style.background = "hotpink";
+//   var a = document.getElementById("inner").firstChild;
+//   var a = document.getElementById("inner").lastChild;
+//   var a = document.getElementById("child-C").lastChild;
+
+//   document.getElementById("child-C").previousElementSibling.style.background = "purple"
+//   document.getElementById("child-C").nextElementSibling.style.background = "orange";
+//   var a = document.getElementById("child-C").previousElementSibling.innerHTML;
+//   var a = document.getElementById("child-C").nextElementSibling;
+//   document.getElementById("child-E").nextElementSibling.style.background = "gold"
+//   document.getElementById("child-head").previousElementSibling.style.background = "gold"
+//   var a = document.getElementById("child-E").nextElementSibling;
+//   var a = document.getElementById("child-head").previousElementSibling;
+
+//   var a = document.getElementById("child-C").previousSibling;
+     var a = document.getElementById("child-C").nextSibling;
+
+
+console.log(a);
